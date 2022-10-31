8d0b2c7c42b77ca55d54493746e66731e5167ceb
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2022-10-31 23:11:01 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from LinkedList.LinkedList import LinkedList
+from LinkedList.Node import Node
+
+
+def main():
+    node = Node()
+    myLL = LinkedList()
+
+    print("Everything is ok")
+
+
+if __name__ == "__main__":
+    main()
