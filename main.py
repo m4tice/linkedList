c60c844909fb6e53f1b0341ad82c45533cd51a8b
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2022-10-31 23:25:08 +0700

    LinkedList

--- main.py
+++ main.py
@@ -4,15 +4,9 @@ from LinkedList.LinkedList import LinkedList
 from LinkedList.Node import Node
 
 
-"""
-main function that executes the program
-
-no input
-@return: nothing
-"""
 def main():
-    node = Node()
-    myLL = LinkedList()
+    # node = Node()
+    # myLL = LinkedList()
 
     print("Everything is ok")
 
