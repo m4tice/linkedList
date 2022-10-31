0462255cbc6ae3aca453360fa1ac14e6f0883a26
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2022-10-31 23:16:06 +0700

    LinkedList

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -1,3 +1,9 @@
+from platform import node
+from Node import Node
+
 class LinkedList:
     def __init__(self):
-        pass
+        self.node = Node()
+        self.head = node
+        self.tail = node
+        self.length = 1
